d3e05682a4220218bcb9af0978497ed1296adda8
pgeovany <pgeovany@dcc.ufrj.br>
2022-05-12 16:34:29 -0300

    fixing stuff

--- src/Modules/App.js
+++ src/Modules/App.js
@@ -1,9 +1,14 @@
+import React from "react";
 import Init from "./Init";
+import MainScreen from "./MainScreen";
 
 export default function App() {
+
+    const [qual, setQual] = React.useState("ok");
+
     return (
         <>
-            <Init />
+           <Init />
         </>
     );
 }
