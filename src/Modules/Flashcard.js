063078f5c270ddf4ef611ea8da77b0972d8066a4
pgeovany <pgeovany@dcc.ufrj.br>
2022-05-14 21:19:26 -0300

    all the flashcard features were implemented

--- src/Modules/Flashcard.js
+++ src/Modules/Flashcard.js
@@ -5,7 +5,9 @@ import flip_card from "../assets/images/vector.svg"
 export default function Flashcard({questionNumber, question, answer}) {
 
     const [expanded, setExpanded] = React.useState(false);
-    const [flipped, setFlipped] = React.useState(false);
+    const [flipped, setFlipped] = React.useState(true);
+    const [recallType, setRecallType] = React.useState("");
+    const [icon, setIcon] = React.useState("play-outline");
     //console.log(questionNumber, question, answer);
     return (
         <>
@@ -13,10 +15,22 @@ export default function Flashcard({questionNumber, question, answer}) {
                 expanded ? 
 
                     flipped ? 
-                    <div className="expandedFlashcard">
+                    <div className="flippedCard">
                         <h1>
                             {answer}
                         </h1>         
+                        <div>
+                            <button className="noRecall" onClick={() => recall("noRecall",setExpanded ,setRecallType, setIcon)}>
+                                <p>Não</p>
+                                <p>lembrei</p>
+                            </button>
+                            <button className="partialRecall" onClick={() => recall("partialRecall",setExpanded ,setRecallType, setIcon)}>
+                                Quase não lembrei
+                            </button>
+                            <button className="totalRecall" onClick={() => recall("totalRecall",setExpanded ,setRecallType, setIcon)}>
+                                Zap!
+                            </button>
+                        </div>
                     </div>
                     :
                     <div className="expandedFlashcard">
@@ -27,11 +41,29 @@ export default function Flashcard({questionNumber, question, answer}) {
                     </div>
 
                 :
-                <div className="flashcard" onClick={() => setExpanded(true)}>
+                <div className={`flashcard ${recallType}`} onClick={() => setExpanded(true)}>
                     <h1>Pergunta {questionNumber}</h1>
-                    <ion-icon name="play-outline"></ion-icon>
+                    <ion-icon name={icon}></ion-icon>
                 </div>
             }
         </>
     );
+}
+
+function recall(answerType, setExpanded, setRecallType, setIcon) {
+    switch (answerType){
+        case "noRecall":
+            setIcon("close-circle");
+            break;
+        case "partialRecall":
+            setIcon("help-circle");
+            break;
+        case "totalRecall":
+            setIcon("checkmark-circle");
+            break;
+        default:
+            break;
+    }
+    setRecallType(answerType);
+    setExpanded(false);
 }
